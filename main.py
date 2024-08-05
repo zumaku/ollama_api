328ca27443a7637f3eaf93eb9cd8035c93ae8383
zumaku <60200121101@uin-alauddin.ac.id>
2024-08-05 11:37:35 +0800

    :(

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
-from typing import Union
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
-from fastapi.responses import StreamingResponse
-import json
+# Artikel dari
+# https://dev.to/jayantaadhikary/using-the-ollama-api-to-run-llms-and-generate-responses-locally-18b7
+
 import requests
+import json
 
-app = FastAPI(debug=True)
+url = "http://localhost:11434/api/generate"
 
-class Item(BaseModel):
-    model: str
-    prompt: str
-
-urls = ["http://localhost:11434/api/generate"]
-
-headers = {
+header = {
     "Content-Type": "application/json"
 }
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+data = {
+    "model": "llama3",
+    "prompt": "Why is the sky blue?",
+    "stream": False
+}
 
-@app.post("/chat/{llms_name}")
-async def update_item(llms_name: str, item: Item):
-    if llms_name == "llama3":
-        url = urls[0]
-        payload = {
-            "model": "llama3",
-            "prompt": item.prompt,
-            "stream": True
-        }
-        
-        def generate():
-            with requests.post(url, headers=headers, json=payload, stream=True) as response:
-                if response.status_code == 200:
-                    for line in response.iter_lines():
-                        if line:
-                            yield line.decode('utf-8')
-                else:
-                    yield f"Error: {response.status_code} - {response.text}"
-        
-        return StreamingResponse(generate(), media_type="text/plain")
-    
-    return {"item_name": item.model, "llms_name": llms_name}
+# response = requests.post(url, headers=header, data=json.dumps(data))
+response = requests.post(url, headers=header, json=data)
+
+if response.status_code == 200:
+    response_text = response.text
+    data = json.loads(response_text)
+    actual_response = data["response"]
+    print(actual_response)
+else:
+    print("Error:", response.status_code, response.text)
